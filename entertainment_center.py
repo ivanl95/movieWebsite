8f4c28a8ab1ca5e53be24fbcfb65fb86bf62f552
Ivan Lara <ivanlara95@gmail.com>
2017-07-11 22:50:35 +0000

    The last movie poster has been updated

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,7 +2,9 @@ import mediaweb
 import fresh_tomatoes
 
 toy_story = mediaweb.Movie("Toy Story", "A story of a boy with toys",
-                           # Code utilizes Google's URL Shortener
+                           ''' 
+                           Code utilizes Google's URL Shortener
+                           '''
                            "https://goo.gl/5Q8xYU",
                            "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
@@ -12,7 +14,7 @@ avatar = mediaweb.Movie("Avatar", "Movie about a marine on an alien planet",
 
 bvs = mediaweb.Movie("Batman v Superman: Dawn of Justice",
                      "A movie about two superheroes fighting",
-                     "https://goo.gl/mhtwzg0",
+                     "https://goo.gl/4riooD",
                      "https://www.youtube.com/watch?v=0WWzgGyAH6Y")
 
 '''
